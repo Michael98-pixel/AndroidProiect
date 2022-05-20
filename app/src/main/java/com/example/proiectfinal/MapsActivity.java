efbcea49d9805b655bd383338a8f2c867987dbad
Michael98-pixel <2v4TE7X6>
2022-05-20 13:11:51 +0300

    Added BAD ROAD function! That's all folks !

--- app/src/main/java/com/example/proiectfinal/MapsActivity.java
+++ app/src/main/java/com/example/proiectfinal/MapsActivity.java
@@ -46,7 +46,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
 
-
 @SuppressWarnings("deprecation")
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
         LocationListener,GoogleApiClient.ConnectionCallbacks,
@@ -70,7 +69,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         assert mapFragment != null;
         mapFragment.getMapAsync(this);
-
+        //Adaugam sensorul de acceleratie
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         Objects.requireNonNull(mSensorManager).registerListener(mSensorListener, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
                 SensorManager.SENSOR_DELAY_NORMAL);
@@ -134,10 +133,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mGoogleApiClient.connect();
     }
 
-
     @Override
     public void onConnected(Bundle bundle) {
-
         mLocationRequest = new LocationRequest();
         mLocationRequest.setInterval(1000);
         mLocationRequest.setFastestInterval(1000);
@@ -150,7 +147,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         tvDistanceDuration = findViewById(R.id.tv_distance_time);
         markerPoints = new ArrayList<>();
         map.setOnMapClickListener(point -> {
-
             // Already two locations
             if(markerPoints.size()>1){
                 markerPoints.clear();
@@ -159,13 +155,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
             // Adding new item to the ArrayList
             markerPoints.add(point);
-
             // Creating MarkerOptions
             MarkerOptions options = new MarkerOptions();
-
             // Setting the position of the marker
             options.position(point);
-
             if(markerPoints.size()==1){
                 options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
             }else if(markerPoints.size()==2){
@@ -185,12 +178,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         });
     }
-
     @Override
     public void onConnectionSuspended(int i) {
     }
-
-
     @Override
     public void onLocationChanged(Location location) {
         mLastLocation = location;
@@ -202,33 +192,23 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                speedtextView.setText(String.format("Current speed: %s km/h", location.getSpeed() * 3600 / 1000));
            }
     }
-
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
     }
     private String getDirectionsUrl(LatLng origin,LatLng dest){
-
         // Origin of route
         String str_origin = "origin="+origin.latitude+","+origin.longitude;
-
         // Destination of route
         String str_dest = "destination="+dest.latitude+","+dest.longitude;
-
         // Sensor enabled
         //String sensor = "sensor=false";
-
         // Building the parameters to the web service
         String parameters = str_origin+"&"+str_dest;
-
         // Output format
         String output = "json";
-
         // Building the url to the web service
-
         return "https://maps.googleapis.com/maps/api/directions/"+output+"?"+parameters+"&key=AIzaSyDOVhiZjl5KIpG5BmLWulad1g1EwCeQ9dg";
     }
-
     /** A method to download json data from url */
     private String downloadUrl(String strUrl) throws IOException {
         String data = "";
@@ -236,20 +216,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         HttpURLConnection urlConnection = null;
         try{
             URL url = new URL(strUrl);
-
             // Creating an http connection to communicate with url
             urlConnection = (HttpURLConnection) url.openConnection();
-
             // Connecting to url
             urlConnection.connect();
-
             // Reading data from url
             iStream = urlConnection.getInputStream();
-
             BufferedReader br = new BufferedReader(new InputStreamReader(iStream));
-
             StringBuilder sb  = new StringBuilder();
-
             String line;
             while( ( line = br.readLine())  != null){
                 sb.append(line);
@@ -275,10 +249,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Downloading data in non-ui thread
         @Override
         protected String doInBackground(String... url) {
-
             // For storing data from web service
             String data = "";
-
             try{
                 // Fetching the data from web service
                 data = downloadUrl(url[0]);
@@ -287,27 +259,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
             return data;
         }
-
         // Executes in UI thread, after the execution of
         // doInBackground()
         @Override
         protected void onPostExecute(String result) {
             super.onPostExecute(result);
-
             ParserTask parserTask = new ParserTask();
-
             // Invokes the thread for parsing the JSON data
             parserTask.execute(result);
         }
     }
-
     /** A class to parse the Google Places in JSON format */
     class ParserTask extends AsyncTask<String, Integer, List<List<HashMap<String,String>>> >{
-
         // Parsing the data in non-ui thread
         @Override
         protected List<List<HashMap<String, String>>> doInBackground(String... jsonData) {
-
             JSONObject jObject;
             List<List<HashMap<String, String>>> routes =  null;
 
@@ -322,7 +288,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
             return routes;
         }
-
         // Executes in UI thread, after the parsing process
         @Override
         protected void onPostExecute(List<List<HashMap<String, String>>> result) {
@@ -335,19 +300,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 Toast.makeText(getBaseContext(), "No Points", Toast.LENGTH_SHORT).show();
                 return;
             }
-
             // Traversing through all the routes
             for(int i=0;i<result.size();i++){
                 points = new ArrayList<>();
                 lineOptions = new PolylineOptions();
-
                 // Fetching i-th route
                 List<HashMap<String, String>> path = result.get(i);
-
                 // Fetching all the points in i-th route
                 for(int j=0;j<path.size();j++){
                     HashMap<String,String> point = path.get(j);
-
                     if(j==0){    // Get distance from the list
                         distance = point.get("distance");
                         continue;
@@ -355,22 +316,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                         duration = point.get("duration");
                         continue;
                     }
-
                     double lat = Double.parseDouble(Objects.requireNonNull(point.get("lat")));
                     double lng = Double.parseDouble(Objects.requireNonNull(point.get("lng")));
                     LatLng position = new LatLng(lat, lng);
-
                     points.add(position);
                 }
-
                 // Adding all the points in the route to LineOptions
                 lineOptions.addAll(points);
                 lineOptions.width(2);
                 lineOptions.color(Color.RED);
             }
-
             tvDistanceDuration.setText(String.format("Distance:%s, Duration:%s", distance, duration));
-
             // Drawing polyline in the Google Map for the i-th route
             map.addPolyline(lineOptions);
         }
