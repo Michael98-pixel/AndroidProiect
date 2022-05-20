5a3fea363ce1b78d97afc87d0a581100ed51df99
Michael98-pixel <2v4TE7X6>
2022-05-20 17:37:15 +0300

    Added BAD ROAD function! That's all folks !

--- app/src/main/java/com/example/proiectfinal/MapsActivity.java
+++ app/src/main/java/com/example/proiectfinal/MapsActivity.java
@@ -323,7 +323,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 }
                 // Adding all the points in the route to LineOptions
                 lineOptions.addAll(points);
-                lineOptions.width(2);
+                lineOptions.width(10);
                 lineOptions.color(Color.RED);
             }
             tvDistanceDuration.setText(String.format("Distance:%s, Duration:%s", distance, duration));
